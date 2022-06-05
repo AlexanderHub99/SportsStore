e33b75845e299d4442a05494c38c6c847c9b81ab
Sasha1448 <agames1448@gmail.com>
2022-06-05 13:08:45 +0300

    [Added] Adding a controller and action method

---
+++ SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers;
+
+public class ProductController : Controller
+{
+    private readonly IProductRepository _productRepository;
+
+    public ProductController(IProductRepository logger)
+    {
+        _productRepository = logger;
+    }
+
+    public ViewResult List() => View(_productRepository.Products);
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+}
