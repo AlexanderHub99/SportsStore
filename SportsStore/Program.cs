55d1d72534250af33a2f1bd4411438c02261099b
Sasha1448 <agames1448@gmail.com>
2022-06-05 17:22:48 +0300

    [ref] Database connection string

--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
-    builder.Configuration.GetConnectionString("ConnectionString")));
+    builder.Configuration.GetConnectionString("sqlConnection")));
 builder.Services.AddTransient<IProductRepository, EFProductRepository>();
 
 var app = builder.Build();
