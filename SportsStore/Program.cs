ff840f8dc4cf758f5e69d786ea5fd0c1d1471b84
Sasha1448 <agames1448@gmail.com>
2022-06-05 12:47:06 +0300

    [Added] storage service registration

--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -1,6 +1,9 @@
+using SportsStore.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddTransient<IProductRepository, FakeProductRepository>();
 
 var app = builder.Build();
 
