a80579760e6be4342ae238f6f59592694c05b88a
Sasha1448 <agames1448@gmail.com>
2022-06-05 18:15:40 +0300

    [Added Db] automatically populate the database if it is empty

--- SportsStore/Models/EFProductRepository.cs
+++ SportsStore/Models/EFProductRepository.cs
@@ -3,10 +3,14 @@
     public class EFProductRepository : IProductRepository
     {
         private readonly ApplicationDbContext _dbContext;
-
         public EFProductRepository(ApplicationDbContext dbContext)
         {
             _dbContext = dbContext;
+            
+            if (!_dbContext.Products.Any())
+            {
+                 SeedData.EnsrurePopulated(dbContext);
+            }
         }
 
         public IEnumerable<Product> Products => _dbContext.Products;
