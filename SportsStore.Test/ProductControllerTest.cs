12d64c644ad49003fd3c9490bc791780347bb8a5
Sasha1448 <agames1448@gmail.com>
2022-06-08 01:45:43 +0300

    [Added test] new test method

--- SportsStore.Test/ProductControllerTest.cs
+++ SportsStore.Test/ProductControllerTest.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using Moq;
 using SportsStore.Controllers;
 using SportsStore.Models;
+using SportsStore.ViewModels;
 using Xunit;
 
 namespace SportsStore.Test
@@ -24,12 +25,35 @@ namespace SportsStore.Test
             ProductController controller = new ProductController(mock.Object);
             controller.pageSixe = 3;
 
-            IEnumerable<Product>? result = controller.List(2).ViewData.Model as IEnumerable<Product>;
+            ProductListViewModel? result = controller.List(2).ViewData.Model as ProductListViewModel;
 
-            Product[] productArrey = result.ToArray();
+            Product[] productArrey = result.Products.ToArray();
             Assert.True(productArrey.Length == 2);
             Assert.Equal("P4", productArrey[0].Name);
             Assert.Equal("P5", productArrey[1].Name);
+        }
+        [Fact]
+        public void Can_Send_Pagination_View_Model()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"},
+                new Product {ProductID = 4, Name = "P4"},
+                new Product {ProductID = 5, Name = "P5"},
+            });
+            ProductController controller = new ProductController(mock.Object);
+            controller.pageSixe = 3;
+
+            ProductListViewModel? result = controller.List(2).ViewData.Model as ProductListViewModel;
+
+            PagingInfo productArrey = result.pagingInfo;
+            Assert.Equal(2, productArrey.CurrentPage);
+            Assert.Equal(3, productArrey.InemsPerPage);
+            Assert.Equal(5, productArrey.TotalItems);
+            Assert.Equal(2, productArrey.TotalPages);
         }
     }
 }
