312cbe4d893a654554d6cc19307371ecc929971e
Sasha1448 <agames1448@gmail.com>
2022-06-07 23:16:52 +0300

    [Added] descriptive class PageLinkTagHelper.cs

---
+++ SportsStore/Infrastructure/PageLinkTagHelper.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using SportsStore.ViewModels;
+
+namespace SportsStore.Infrastructure
+{
+    [HtmlTargetElement("div", Attributes = "page-models")]
+    public class PageLinkTagHelper : TagHelper
+    {
+        private IUrlHelperFactory _helperFactory;
+
+        public PageLinkTagHelper(IUrlHelperFactory urlHelperFactory)
+        {
+            _helperFactory = urlHelperFactory;
+        } 
+        
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public  ViewContext ViewContext { get; set; }
+        public PagingInfo PageModel { get; set; }
+        public string PagwAction { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            IUrlHelper urlHelper = _helperFactory.GetUrlHelper(ViewContext);
+            TagBuilder result = new TagBuilder("div");
+            for (int i = 1; i <-PageModel.TotalItems; i++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+                tag.Attributes["href"] = urlHelper.Action(PagwAction, new {page = i});
+                tag.InnerHtml.Append(i.ToString());
+                result.InnerHtml.AppendHtml(tag);
+            }
+
+            output.Content.AppendHtml(result.InnerHtml);
+        }
+        
+    }
+}
+
