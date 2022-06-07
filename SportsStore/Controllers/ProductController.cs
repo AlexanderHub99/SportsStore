411081b89e3586138510745e514bcc28678d585a
Sasha1448 <agames1448@gmail.com>
2022-06-07 22:22:47 +0300

    [Ref Controller] Added pagination support

--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -2,16 +2,23 @@
 using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
 
-namespace SportsStore.Controllers;
-
-public class ProductController : Controller
+namespace SportsStore.Controllers
 {
-    private readonly IProductRepository _productRepository;
-
-    public ProductController(IProductRepository logger)
+    public class ProductController : Controller
     {
-        _productRepository = logger;
-    }
+        private readonly IProductRepository _productRepository;
+        
+        public ProductController(IProductRepository logger)
+        {
+            _productRepository = logger;
+        }
 
-    public ViewResult List() => View(_productRepository.Products);
+        private readonly int _pageSixe = 4;
+    
+        public ViewResult List(int page = 1) => View(_productRepository.Products
+            .OrderBy(p => p.ProductID)
+            .Skip((page -1)*_pageSixe)
+            .Take(_pageSixe));
+    }
 }
+
