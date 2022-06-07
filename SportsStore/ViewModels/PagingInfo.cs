77b76bdac4f20e4dd5b9eaadff588eb4b13785c6
Sasha1448 <agames1448@gmail.com>
2022-06-08 01:10:27 +0300

    [ref]

--- SportsStore/ViewModels/PagingInfo.cs
+++ SportsStore/ViewModels/PagingInfo.cs
@@ -5,7 +5,7 @@
         public int TotalItems { get; set; }
         public int InemsPerPage { get; set; }
         
-        public int CurrentPage { get; set; }
+        public int CurrentPage { get;set; }
         
         public int TotalPage => (int) Math.Ceiling((decimal) TotalItems / InemsPerPage);
     }
