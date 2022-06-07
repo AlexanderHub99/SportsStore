65a1df4a4a22b06c71bd4a37cddb789bc8ad90aa
Sasha1448 <agames1448@gmail.com>
2022-06-07 22:50:16 +0300

    [Added Model] helper model for page count

---
+++ SportsStore/ViewModels/PagingInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace SportsStore.ViewModels
+{
+    public class PagingInfo
+    {
+        public int TotalItems { get; set; }
+        public int InemsPerPage { get; set; }
+        
+        public int TotalPage => (int) Math.Ceiling((decimal) TotalItems / InemsPerPage);
+    }
+}
+
