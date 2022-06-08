7c8cc9993ab474b1adee184e8e52340dbf7aa731
Sasha1448 <agames1448@gmail.com>
2022-06-08 20:00:29 +0300

    [Added class css]

--- SportsStore/Infrastructure/PageLinkTagHelper.cs
+++ SportsStore/Infrastructure/PageLinkTagHelper.cs
@@ -23,8 +23,15 @@ namespace SportsStore.Infrastructure
         [HtmlAttributeNotBound]
         public  ViewContext ViewContext { get; set; }
         public string TotalPages { get; set; }
+        public string CurrentPage { get; set; }
         public string PageAction { get; set; }
-
+        
+        
+        public bool  PageClassesEnabled { get; set; }
+        public string PageClass { get; set; }
+        public string PageClassNormal { get; set; }
+        public string PageClassSelected { get; set; }
+        
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             IUrlHelper urlHelper = _helperFactory.GetUrlHelper(ViewContext);
@@ -33,6 +40,11 @@ namespace SportsStore.Infrastructure
             {
                 TagBuilder tag = new TagBuilder("a");
                 tag.Attributes["href"] = urlHelper.Action(PageAction, new {page = i});
+                if (PageClassesEnabled)
+                {
+                    tag.AddCssClass(PageClass);
+                    tag.AddCssClass(i == Convert.ToInt32(CurrentPage) ? PageClassSelected:PageClassNormal);
+                }
                 tag.InnerHtml.Append(i.ToString());
                 result.InnerHtml.AppendHtml(tag);
             }
