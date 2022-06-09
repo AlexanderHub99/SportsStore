4f55bdf9135073b5c27f3e99b21e6a7aaffb099e
Sasha1448 <agames1448@gmail.com>
2022-06-10 02:27:15 +0300

    [Added] category list generator

---
+++ SportsStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SportsStore.Models;
+
+namespace SportsStore.Components
+{
+    public class NavigationMenuViewComponent: ViewComponent
+    {
+        private IProductRepository _productRepository;
+
+        public NavigationMenuViewComponent(IProductRepository repository)
+        {
+            _productRepository = repository;
+        }
+
+        //Кудато потерялася мето файл :(                                                                    :todo
+        // ReSharper disable once Mvc.ViewComponentViewNotResolved                                          :todo
+        public IViewComponentResult Invoke() => View(_productRepository.Products
+            .Select(x => x.Category)
+            .Distinct()
+            .OrderBy(x => x));
+    }
+}
+
