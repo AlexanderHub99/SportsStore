a15342958e101b662f63026fea203e21ac5afa2b
Sasha1448 <agames1448@gmail.com>
2022-06-17 01:25:42 +0300

    [Added] Shopping cart

---
+++ SportsStore/ViewModels/CatrIndexViewModels.cs
@@ -0,0 +1,9 @@
+﻿namespace SportsStore.Models.ViewModels
+{
+    public class CatrIndexViewModels
+    {
+        public Cart? Cart { get; set; }
+        public string? ReturnUrl { get; set; }
+    }
+}
+
