359696f369aceedfcb333ea39a303bd5d0afd858
Sasha1448 <agames1448@gmail.com>
2022-06-19 12:34:40 +0300

    [ref] Added service cart

--- SportsStore/Controllers/CartController.cs
+++ SportsStore/Controllers/CartController.cs
@@ -5,59 +5,47 @@ using SportsStore.Models.ViewModels;
 
 namespace SportsStore.Controllers
 {
- public class CartController : Controller
- {
-     private IProductRepository _repository;
- 
-     public CartController(IProductRepository repo)
-     {
-         _repository = repo;
-     }
- 
-     public ViewResult Index(string returnUrl)
-     {
-         return View(new CatrIndexViewModels
-         {
-             Cart = GetCart(),
-             ReturnUrl = returnUrl
-         });
-     }
-     
-     public RedirectToActionResult AddToCart(int productId , string returnUrl)
-     {
-         Product? product = _repository.Products
-             .FirstOrDefault(p => p.ProductID == productId);
-         if (product!= null)
-         {
-             Cart cart = GetCart();
-             cart.AddItem(product, 1);
-             SaveCart(cart);
-         }
-         return RedirectToAction("Index", new {returnUrl});
-     }
- 
-     public RedirectToActionResult RemoveFromCart(int productID, string returnUrl)
-     {
-         Product? product = _repository.Products.FirstOrDefault(p => p.ProductID == productID);
-         if (product != null)
-         {
-             Cart cart = GetCart();
-             cart.RemoveLine(product);
-             SaveCart(cart);
-         }
-         return RedirectToAction("Index", new {returnUrl});
-     }
-     private Cart GetCart()
-     {
-         Cart cart = HttpContext.Session.GetJson<Cart>("Cart")?? new Cart();
-         return cart;
-     }
-           
-     
-     private void SaveCart(Cart cart)
-     {
-         HttpContext.Session.SetJson("Cart", cart);
-     }
-     
- }   
+    public class CartController : Controller
+    {
+        private readonly IProductRepository _repository;
+        private readonly Cart _cart;
+
+        public CartController(IProductRepository repo, Cart cart)
+        {
+            _repository = repo;
+            _cart = cart;
+        }
+
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CatrIndexViewModels
+            {
+                Cart = _cart,
+                ReturnUrl = returnUrl
+            });
+        }
+
+        public RedirectToActionResult AddToCart(int productId, string returnUrl)
+        {
+            Product? product = _repository.Products
+                .FirstOrDefault(p => p.ProductID == productId);
+            if (product != null)
+            {
+                _cart.AddItem(product, 1);
+            }
+
+            return RedirectToAction("Index", new {returnUrl});
+        }
+
+        public RedirectToActionResult RemoveFromCart(int productID, string returnUrl)
+        {
+            Product? product = _repository.Products.FirstOrDefault(p => p.ProductID == productID);
+            if (product != null)
+            {
+                _cart.RemoveLine(product);
+            }
+
+            return RedirectToAction("Index", new {returnUrl});
+        }
+    }
 }
