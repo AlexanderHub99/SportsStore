69a74c4cd5d5884cacdc571ff2a3f31b932e7de1
Sasha1448 <agames1448@gmail.com>
2022-06-29 16:04:50 +0300

    [added]  CartSummaryViewComponent

---
+++ SportsStore/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SportsStore.Models;
+
+namespace SportsStore.Components
+{
+    public class CartSummaryViewComponent: ViewComponent
+    {
+        public Cart cart;
+        public CartSummaryViewComponent(Cart cart)
+        {
+            this.cart = cart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+}
+
